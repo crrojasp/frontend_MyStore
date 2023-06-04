165c7bd2681d30a175f222031fdfe0ea545a27dc
dbellol <42379685+dbellol@users.noreply.github.com>
2023-06-04 10:59:57 -0500

    Add files via upload

--- VistasDiana/Somos.js
+++ VistasDiana/Somos.js
@@ -10,25 +10,26 @@ const Somos = () => {
 
   return (
     <section className="section-white">
-    <h2 className='Ourteam'>¿Quiénes somos?</h2>
+    <div> <h1 className='Ourteam'>¿Quiénes somos?</h1>
       <div className="team-members">
-      <div className="Diana">
-        <img src={Diana} alt="Diana" />
+        <div className="Diana">
+          <img src={Diana} alt="Diana" />
             <h3>Diana M. Bello L.</h3>
             <h4>FrontEnd</h4>
-            <p className='descripcion'> Estudiante de la UN sede Boogotá, centrada en el front</p>
-      </div>
-      <div className="Julian">
-        <img src={Julian} alt="Julian" />
-          <h3>Julian Fernández</h3>
-          <p className='descripcion'> Estudiante de la UN sede Boogotá, centrado en el desarrollo FullStack</p>
-          <p>FullStack</p>
-      </div>
-      <div className="Cristian">
-        <img src={Cristian} alt="Cristian" />
-          <h3>Cristian A. Rojas</h3>
-          <p className='descripcion'> Estudiante de la UN sede Boogotá, centrado en el desarrollo FullStack</p>
-          <p>FullStack</p>
+            <p className='descripcion'> Estudiante de la UN sede Bogotá, centrada en el front</p>
+        </div>
+        <div className="Julian">
+          <img src={Julian} alt="Julian" />
+            <h3>Julian Fernández</h3>
+            <p className='descripcion'> Estudiante de la UN sede Bogotá, centrado en el desarrollo FullStack</p>
+            <p>FullStack</p>
+        </div>
+        <div className="Cristian">
+          <img src={Cristian} alt="Cristian" />
+            <h3>Cristian A. Rojas</h3>
+            <p className='descripcion'> Estudiante de la UN sede Bogotá, centrado en el desarrollo FullStack</p>
+            <p>FullStack</p>
+        </div>
       </div>
     </div>
     </section>
