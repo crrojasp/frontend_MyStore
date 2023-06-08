a04671f302bae45d8c317400c7aa174cc48b1a62
crrojasp <64115649+crrojasp@users.noreply.github.com>
2023-06-07 22:29:02 -0500

    Update Registro.jsx

--- src/vistas/Registro.jsx
+++ src/vistas/Registro.jsx
@@ -4,48 +4,23 @@ import './../Estilos/Registro.css';
 import { Link } from "react-router-dom";
 
 const Registro = () => {
-    const [hoveredComprador, setHoveredComprador] = useState(false);
-    const [hoveredVendedor, setHoveredVendedor] = useState(false);
-
-    const handleMouseEnterComprador = () => {
-        setHoveredComprador(true);
-    };
-
-    const handleMouseLeaveComprador = () => {
-        setHoveredComprador(false);
-    };
-
-    const handleMouseEnterVendedor = () => {
-        setHoveredVendedor(true);
-    };
-
-    const handleMouseLeaveVendedor = () => {
-        setHoveredVendedor(false);
-    };
-
     return (
-        <div className="h-screen flex items-center justify-center bg-white">
-            <div className="flex space-x-10 mx-4">
-                <Link
-                    to="/RegistroComprador"
-                    onMouseEnter={handleMouseEnterComprador}
-                    onMouseLeave={handleMouseLeaveComprador}
-                    className={`${hoveredComprador ? "bg-purple-700 transform hover:scale-105" : "bg-purple-500 transform hover:scale-100"
-                        } hover:bg-purple-700 text-white font-bold py-4 px-8 rounded-full transition-all duration-300`}
-                >
-                    <span className="text-lg">Comprador</span>
+        
+        <div className="contRegistro">
+            <label className="Bienvenido">
+            Bienvenido a MyStore, elija por favor la opción que desee:
+            </label>
+            <div className="container">
+
+                <Link to="/RegistroComprador" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Comprador
+                </Link><br></br>
+                <Link to="/RegistroVendedor" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Vendedor
                 </Link>
-                <Link
-                    to="/RegistroVendedor"
-                    onMouseEnter={handleMouseEnterVendedor}
-                    onMouseLeave={handleMouseLeaveVendedor}
-                    className={`${hoveredVendedor ? "bg-purple-700 transform hover:scale-105" : "bg-purple-500 transform hover:scale-100"
-                        } hover:bg-purple-700 text-white font-bold py-4 px-8 rounded-full transition-all duration-300`}
-                >
-                    <span className="text-lg">Vendedor</span>
-                </Link>
+
             </div>
-        </div>
+        </div>  
     );
 };
 
